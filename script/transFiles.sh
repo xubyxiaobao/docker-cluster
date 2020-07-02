19efd64a8df8e30f66ac7b95a6bf9df121da60c2
xutao <18510328832@163.com>
2020-07-02 08:48:08 +0800

    init

--- script/transFiles.sh
+++ script/transFiles.sh
@@ -2,15 +2,16 @@
 
 dir=$(cd $(dirname evn.sh);pwd)
 
-include_files="evn.sh,start.sh,stop.sh"
-desc_path="root@192.168.33.21:/opt/docker/"
+include_files="evn.sh,start.sh,stop.sh,../mongo"
+#desc_path="root@192.168.33.21:/opt/docker/"
+desc_path="root@192.168.1.181:/opt/docker/"
 
 OLD_IFS="$IFS"
 IFS=","
 arr=($include_files)
 for file in ${arr[@]} ; do
     absolute_path=${dir}/${file}
-    scp $absolute_path $desc_path
+    scp -r $absolute_path $desc_path
     if [ $? == 0 ]; then
         echo "${absolute_path} upload success!"
     else
